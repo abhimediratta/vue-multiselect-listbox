ec55d9b0a6b2dc188a61cb274f032ea57862a969
Abhi <abhimediratta@gmail.com>
2022-10-10 23:33:09 +0530

    WIP: Move Multi-select to typescript

---
+++ src/components/MultiSelect-v3.vue
@@ -0,0 +1,83 @@
+<script setup lang="ts">
+import { ListItem, ItemCallback } from './main';
+
+interface Props {
+  modelValue: Array<ListItem>;
+  options: Array<ListItem>;
+  reduceDisplayProperty: ItemCallback;
+  reduceValueProperty: ItemCallback;
+  searchOptionsPlaceholder: string;
+  selectedOptionsPlaceholder: string;
+  noOptionsText: string;
+  selectedNoOptionsText: string;
+  noOptionsFoundText: string;
+  noSelectedOptionsFoundText: string;
+  showSelectAllButtons: boolean;
+  highlightDiff: boolean;
+  searchInputClass: string;
+  highlightRemovedClass: string;
+  highlightAddedClass: string;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  
+});
+</script>
+
+<template>
+  <div class="msl-multi-select">
+    <SearchableList
+      :list-items="availableOptions"
+      :no-options-text="noOptionsText"
+      :no-items-found-text="noOptionsFoundText"
+      :selected-list-items="selectedItems"
+      :display-property="reduceDisplayProperty"
+      :value-property="reduceValueProperty"
+      :placeholder-text="searchOptionsPlaceholder"
+      :search-input-class="searchInputClass"
+      :highlight-class="highlightRemovedClass"
+      :highlight-items="newlyRemovedItems"
+      class="msl-multi-select__list"
+      :highlight-diff="highlightDiff"
+      @on-click-option="onOptionSelect"
+    />
+
+    <div class="msl-multi-select__actions">
+      <a
+        class="msl-multi-select__action msl-multi-select__action-select-all"
+        :class="{'invisible': !showSelectAllButtons}"
+        @click="onSelectAllOptions"
+      >
+        <font-awesome-icon icon="angle-double-right" />
+      </a>
+
+      <font-awesome-icon
+        icon="exchange-alt"
+        class="multi-select__action-icon"
+      />
+
+      <a
+        class="msl-multi-select__action msl-multi-select__action-unselect-all"
+        :class="{'invisible': !showSelectAllButtons}"
+        @click="onUnselectAllOptions"
+      >
+        <font-awesome-icon icon="angle-double-left" />
+      </a>
+    </div>
+
+    <SearchableList
+      :list-items="selectedItems"
+      :no-options-text="selectedNoOptionsText"
+      :no-items-found-text="noSelectedOptionsFoundText"
+      :display-property="reduceDisplayProperty"
+      :value-property="reduceValueProperty"
+      :placeholder-text="selectedOptionsPlaceholder"
+      :search-input-class="searchInputClass"
+      :highlight-class="highlightAddedClass"
+      :highlight-items="newlyAddedItems"
+      :highlight-diff="highlightDiff"
+      class="msl-multi-select__selected msl-multi-select__list"
+      @on-click-option="onOptionRemove"
+    />
+  </div>
+</template>
